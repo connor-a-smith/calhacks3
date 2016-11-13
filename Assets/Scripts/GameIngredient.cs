871cb1f613b8e0c20db300c7086f669969ac2317
Connor Smith <c3smith@ucsd.edu>
2016-11-12 22:29:52 -0800

    Added tooltips for objects

--- Assets/Scripts/GameIngredient.cs
+++ Assets/Scripts/GameIngredient.cs
@@ -1,7 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof (VRTK.VRTK_InteractableObject))]
 public class GameIngredient : MonoBehaviour {
 
     public Ingredient.EnglishName name;
+
+    private Ingredient correspondingIngredient;
+
+    [HideInInspector] public VRTK.VRTK_ObjectTooltip tooltip;
+
+    public void Start() {
+
+        foreach (Ingredient ingredient in RecipeController.instance.allIngredients) {
+
+            if (ingredient.name == name) {
+
+                correspondingIngredient = ingredient;
+                break;
+
+            }
+        }
+
+        GameObject tooltipObject = GameObject.Instantiate(RecipeController.instance.tooltipPrefab) as GameObject;
+
+        tooltipObject.transform.parent = this.gameObject.transform;
+        tooltipObject.transform.localPosition = Vector3.up;
+
+        tooltip = tooltipObject.GetComponent<VRTK.VRTK_ObjectTooltip>();
+        
+        tooltip.displayText = correspondingIngredient.translatedName;
+        tooltip.drawLineTo = this.gameObject.transform;
+        tooltip.gameObject.SetActive(false);
+
+    }
+
+    public void OnTriggerEnter(Collider other) {
+
+        Debug.LogWarning("Entering?");
+
+        SteamVR_TrackedObject controller = other.GetComponent<SteamVR_TrackedObject>();
+
+        if (controller != null) {
+
+            EnableTooltip();
+
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+
+        SteamVR_TrackedObject controller = other.GetComponent<SteamVR_TrackedObject>();
+        if(controller != null) {
+
+            DisableTooltip();
+
+        }
+    }
+
+    public void EnableTooltip() {
+        tooltip.displayText = correspondingIngredient.translatedName;
+
+        tooltip.gameObject.SetActive(true);
+
+    }
+
+    public void DisableTooltip() {
+
+        tooltip.gameObject.SetActive(false);
+
+    }
+
 }
