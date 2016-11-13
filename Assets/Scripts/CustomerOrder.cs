53f55d81ff9ed7b83c291518a12b6179344fdbb6
anikan <anikan33@gmail.com>
2016-11-12 23:56:54 -0800

    Set up some tv stuff

--- Assets/Scripts/CustomerOrder.cs
+++ Assets/Scripts/CustomerOrder.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class CustomerOrder : MonoBehaviour {
 
     private Recipe desiredDish;
     private List<Ingredient.EnglishName> desiredIngredients;
     [SerializeField] private string successString = "Gracias!";
-
-
+    [SerializeField] private Text tvText;
+    
     // Use this for initialization
     void Start () {
         //Get a random dish and ingredients
@@ -27,12 +28,31 @@ public class CustomerOrder : MonoBehaviour {
     {
         desiredDish = RecipeController.instance.recipes[Random.Range(0, RecipeController.instance.recipes.Count)];
 
+        string genderText;
+
+        //una for female items, uno otherwise.
+        if (desiredDish.name[desiredDish.name.Length-1] == 'a')
+        {
+            genderText = "una";
+        }
+
+        else
+        {
+            genderText = "uno";
+        }
+
+
+        tvText.text = "Yo quiero " + genderText + " " + desiredDish.name;
+
         desiredIngredients = new List<Ingredient.EnglishName>();
 
         //Getting a copy of the additional ingredients to avoid duplicates.
         List<Ingredient> ingredientOptions = new List<Ingredient>(RecipeController.instance.additionalIngredients);
 
         int numAdditionalIngredients = Random.Range(0, ingredientOptions.Count);
+
+        print(numAdditionalIngredients);
+
 
         //Loop through the copied array, adding ingredients that haven't been added before.
         for (int i = 0; i < numAdditionalIngredients; i++)
@@ -41,10 +61,10 @@ public class CustomerOrder : MonoBehaviour {
             desiredIngredients.Add(ingredientOptions[randomIndex].name);
             desiredIngredients.RemoveAt(randomIndex);
 
-
+            print(ingredientOptions[randomIndex].name);
         }
+        print(desiredDish.name);
 
-           
     }
 
     void OnColliderEnter(Collision other)
@@ -61,11 +81,12 @@ public class CustomerOrder : MonoBehaviour {
         int score = 0;
         string result;
 
+        StartCoroutine(StartOrderAfterInputSeconds(5f));
                 
         //If the entire food is wrong, then earn 0 points.
         if (food.foodRecipe.name != desiredDish.name)
         {
-            result = "Wrong order!";
+            tvText.text = "Wrong order!";
 
 
             return 0; 
@@ -74,7 +95,7 @@ public class CustomerOrder : MonoBehaviour {
 
         else
         {
-            result = "Good Job!";
+            tvText.text = "Good Job!";
             return 10;
             
         }
@@ -106,5 +127,12 @@ public class CustomerOrder : MonoBehaviour {
 
         }*/
         
+    }
+
+    IEnumerator StartOrderAfterInputSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        SetDishAndIngredients();
     }
 }
--- Assets/Scripts/FinishedFood.cs
+++ Assets/Scripts/FinishedFood.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class FinishedFood : MonoBehaviour {
     [HideInInspector] public List<Ingredient> additionalIngredients;
     [HideInInspector] public Recipe foodRecipe;
-
+    
 	// Use this for initialization
 	void Start () {
 	
